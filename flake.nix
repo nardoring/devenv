0b85955b53dc72625546aa984baef4ebd2b6448f
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-01-28 06:54:38 -0600

    wayland fails to build on unstable

--- flake.nix
+++ flake.nix
@@ -1,14 +1,14 @@
 # /etc/nixos/flake.nix
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-python.url = "github:cachix/nixpkgs-python";
     nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -21,7 +21,7 @@
     ...
   }: {
     nixosConfigurations = {
-      hostname = nixpkgs.lib.nixosSystem {
+      hostname = nixpkgs.lib.nixosSystem { # TODO Set hostname
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
