b75b48ef0eb8ee1f73ac1d8a31579539069cf365
Benjamin Hajdukiewicz <70604257+katanallama@users.noreply.github.com>
2024-02-07 11:31:41 -0600

    factor out common configs

--- users/bh/configuration.nix
+++ users/bh/configuration.nix
@@ -10,15 +10,8 @@
     "${modulesPath}/profiles/minimal.nix"
   ];
 
-  environment.noXlibs = false;
-
   wsl = {
-    # https://nix-community.github.io/NixOS-WSL/options.html
-    # for a comprehensive list of wsl options
-    enable = true;
     defaultUser = "bh";
-    nativeSystemd = true;
-    docker-desktop.enable = true;
   };
 
   users.users.bh = {
@@ -27,21 +20,7 @@
     extraGroups = ["docker"];
   };
 
-  virtualisation.docker.enable = true;
-
   # set this to your Windows 11/10 “Device name.” You can find
   # this in the Windows Settings app under System > About.
   networking.hostName = "NB10798";
-
-  nixpkgs.config.allowUnfree = true;
-
-  # Enable nix flakes
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
-  system.stateVersion = "23.11";
 }
